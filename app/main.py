5ce89bdb36589651403981a091cdbfb8ff799dbf
urimJ <ufrl128@dgu.ac.kr>
2022-05-25 19:32:05 +0900

    MODIFY main.py

--- app/main.py
+++ app/main.py
@@ -10,11 +10,10 @@ def result():
    if request.method == 'POST':
       result = dict()
       result['Name'] = request.form.get('Name')
-
-      # 학번
-      # 성별
-      # 학과
-      # 프로그래밍 언어 -> hint) ','.join(list명)을 사용하면 list 안에 있는 항목들이 ','로 나누어져 출력됨.
+      result['Student Number'] = request.form.get('Student Number')
+      result['Gender'] = request.form.get('Gender')
+      result['Major'] = request.form.get('Major')
+      result['Languages'] = ','.join(request.form.getlist('Programming Languages'))
 
       return render_template("result.html",result = result)
 
